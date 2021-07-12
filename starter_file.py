1122a89d3af2d1023e13140be28a2d8ff24e2300
Mariya <mariyasha432@protonmail.com>
2021-07-12 02:52:16 -0700

    uploading project files

---
+++ starter_file.py
@@ -0,0 +1,16 @@
+from kivymd.uix.screen import MDScreen
+from kivymd.app import MDApp
+from kivy.uix.image import Image
+from kivymd.uix.button import MDFillRoundFlatIconButton, MDFillRoundFlatButton
+from kivymd.uix.textfield import MDTextField
+from kivymd.uix.label import MDLabel
+from kivymd.uix.toolbar import MDToolbar
+
+class ConverterApp(MDApp):
+    def build(self):
+        screen = MDScreen()
+        #UI Widgets go here
+        return screen
+
+if __name__ == '__main__':
+    ConverterApp().run()
